a93e865f92d89c5322b72c72a1449ea5f89fe21b
ProfPaulB <paulbonenfant@hotmail.com>
2021-02-13 16:42:41 -0500

    First Commit

---
+++ src/midtermreviewcodeforpartc/User.java
@@ -0,0 +1,61 @@
+package midtermreviewcodeforpartc;
+
+/**
+ * A class that models a User for Uno Online. The user has a name and a password.
+ *
+ * @author dancye, 2019
+ * @author Paul Bonenfant, 2021
+ */
+public class User {
+
+    private String name;
+    private String password;
+
+    /**
+     * a constructor that takes in the user's name and password
+     *
+     * @param name
+     * @param password
+     */
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    /**
+     * The getter for the user name
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * The setter for the user name
+     *
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * The getter for the password
+     *
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * A setter for the password
+     *
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
